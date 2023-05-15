79526f3151b3b149d9638bddd0464fa04649e428
SplittyDev <splittydev@gmail.com>
2023-05-15 23:34:27 +0200

    Fix header spacing

--- badgers-web/src/app/layout.tsx
+++ badgers-web/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="flex min-h-screen flex-col items-center p-16 gap-8">
-            <header>
+            <header className="flex flex-col gap-4">
                 <h1 className="text-6xl font-bold text-center">
                     Badge
                     <span className="text-orange-500">rs</span>
