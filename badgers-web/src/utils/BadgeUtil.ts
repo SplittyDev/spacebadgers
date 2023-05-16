a37bbedd4698f7b66259e065a2a4f41ae21200e6
SplittyDev <splittydev@gmail.com>
2023-05-16 14:15:05 +0200

    Fix badge response builder

--- badgers-web/src/utils/BadgeUtil.ts
+++ badgers-web/src/utils/BadgeUtil.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 interface BadgeOverrides {
     labelColor?: string,
@@ -7,7 +7,7 @@ interface BadgeOverrides {
 }
 
 export default class BadgeUtil {
-    static async generate(label: string, status: string, overrides: BadgeOverrides = {}): Promise<Response> {
+    static async generate(label: string, status: string, overrides: BadgeOverrides = {}): Promise<NextResponse> {
         const api = {
             proto: process.env.NEXT_PUBLIC_API_PROTO,
             host: process.env.NEXT_PUBLIC_API_HOST,
@@ -20,14 +20,30 @@ export default class BadgeUtil {
             .entries(overrides)
             .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
             .join('&')
-        return await fetch(`${api.proto}://${api.host}/badge/${pathParams.label}/${pathParams.status}?${queryParams}`)
+        const resp = await fetch(`${api.proto}://${api.host}/badge/${pathParams.label}/${pathParams.status}?${queryParams}`)
+        const data = await resp.arrayBuffer()
+        return new NextResponse(data, {
+            status: resp.status,
+            statusText: resp.statusText,
+            headers: {
+                'Content-Type': 'image/svg+xml',
+            }
+        })
     }
 
-    static async passThrough(request: NextRequest) {
+    static async passThrough(request: NextRequest): Promise<NextResponse> {
         const api = {
             proto: process.env.NEXT_PUBLIC_API_PROTO,
             host: process.env.NEXT_PUBLIC_API_HOST,
         }
-        return await fetch(`${api.proto}://${api.host}${request.nextUrl.pathname}${request.nextUrl.search}`)
+        const resp = await fetch(`${api.proto}://${api.host}${request.nextUrl.pathname}${request.nextUrl.search}`)
+        const data = await resp.arrayBuffer()
+        return new NextResponse(data, {
+            status: resp.status,
+            statusText: resp.statusText,
+            headers: {
+                'Content-Type': 'image/svg+xml',
+            }
+        })
     }
 }
