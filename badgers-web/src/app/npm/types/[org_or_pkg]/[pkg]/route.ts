8ccd40676d033a61638582a3168989ed7ed4faf1
SplittyDev <splittydev@gmail.com>
2023-05-21 03:11:01 +0200

    Add npm endpoints

---
+++ badgers-web/src/app/npm/types/[org_or_pkg]/[pkg]/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest } from "next/server"
+
+import { Badge, Npm } from '@/utils'
+
+interface Params {
+    params: {
+        org_or_pkg: string
+        pkg: string
+    }
+}
+
+export async function GET(request: NextRequest, { params: { org_or_pkg: org, pkg } }: Params) {
+    const data = await Npm.getPackageVersion(`${org}/${pkg}`, 'latest')
+    if (data === null) return await Badge.error(request, 'npm')
+    const getTypesText = async (): Promise<"included" | "missing" | string> => {
+        if (data.types) return "included"
+        const typesPackage = await Npm.getTypesPackage(`${org}/${pkg}`)
+        return typesPackage ?? "missing"
+    }
+    const getTypesColor = (types: string): string => {
+        if (types === "included") return "blue"
+        if (types === "missing") return "orange"
+        return "cyan"
+    }
+    const typesText = await getTypesText()
+    const typesColor = getTypesColor(typesText)
+    return await Badge.generate(request, 'types', typesText, {
+        color: typesColor
+    })
+}
--- badgers-web/src/utils/index.ts
+++ badgers-web/src/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as Badge } from './BadgeUtil'
 export { default as GitHub } from './GitHubUtil'
 export { default as Crates } from './CratesUtil'
+export { default as Npm } from './NpmUtil'
