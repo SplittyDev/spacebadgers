af4d7c20332890f4d1aa2a65959950d5fd063c50
SplittyDev <splittydev@gmail.com>
2023-05-24 01:41:53 +0200

    Improve footer markup

--- badgers-web/src/app/layout.tsx
+++ badgers-web/src/app/layout.tsx
@@ -43,25 +43,31 @@ export default function RootLayout({
                 </header>
                 <div className="flex flex-col gap-8 w-full">
                     {children}
-                    <footer className="flex flex-wrap justify-between gap-2 bg-gray-100 mx-4 px-4 py-2 rounded-md">
-                        <div className="text-sm">
-                            <span className="text-gray-600">
-                                Made with <span className="text-rose-500">♥</span> by <a target="_blank" className="text-gray-500 hover:text-amber-600" href="https://github.com/SplittyDev">Marco Quinten</a>.
-                            </span>
-                        </div>
-                        <div className="flex gap-4 text-sm">
-                            <a target="_blank" className="text-gray-500 hover:text-amber-600" href="https://github.com/splittydev/badgers">GitHub</a>
-                        </div>
+                    <footer className="flex flex-col gap-6 w-full">
+
+                        {/* Created by and link section */}
+                        <section className="flex flex-wrap justify-between gap-2 bg-gray-100 mx-4 px-4 py-2 rounded-md">
+                            <div className="text-sm">
+                                <span className="text-gray-600">
+                                    Made with <span className="text-rose-500">♥</span> by <a target="_blank" className="text-gray-500 hover:text-amber-600" href="https://github.com/SplittyDev">Marco Quinten</a>.
+                                </span>
+                            </div>
+                            <div className="flex gap-4 text-sm">
+                                <a target="_blank" className="text-gray-500 hover:text-amber-600" href="https://github.com/splittydev/badgers">GitHub</a>
+                            </div>
+                        </section>
+
+                        {/* Informational Text */}
+                        <section className="flex flex-col gap-4 px-6 text-sm text-justify text-gray-700">
+                            <h2 className="text-md text-gray-600 font-bold">About SpaceBadgers</h2>
+                            <p>Hey there, welcome to SpaceBadgers! If you&#x27;re wondering who we are and what we do, let us fill you in. We&#x27;re an open-source project with a passion for delivering top-notch SVG badges that are as speedy as they are stylish.</p>
+                            <p>You see, we noticed that developers and project maintainers like you needed a way to display key information &#x2013; think build status, version, download counts &#x2013; in a way that&#x27;s quick and easy to integrate. That&#x27;s where we come in.</p>
+                            <p>Under the hood, we&#x27;re powered by a Rust-based core library <a className="text-xs text-gray-700 hover:text-orange-600" href="https://crates.io/crates/spacebadgers" target="_blank">(crates.io)</a> and a Cloudflare worker. This dynamic duo ensures that we&#x27;re always delivering badges with excellent performance, straight from the edge. And speaking of integration, we&#x27;ve made it our mission to ensure smooth sailing when it comes to working with third-party services, courtesy of our NextJS-based API routes running on Vercel edge infrastructure.</p>
+                            <p>What makes us different? We&#x27;re glad you asked! Unlike traditional image-based badges, we&#x27;re all about SVGs. They&#x27;re scalable, high-quality, and we serve them in a minified and compressed form for optimal delivery.</p>
+                            <p>So that&#x27;s us, SpaceBadgers, in a nutshell. We&#x27;re here to provide a seamless, efficient badge generation process. If that sounds like your cup of tea, why not join the open-source community using SpaceBadgers today? We&#x27;d be thrilled to have you onboard!</p>
+                            <p>For more information, take a look at our <a className="text-gray-700 hover:text-orange-600 underline underline-offset-2" href="https://github.com/splittydev/spacebadgers" target="_blank">GitHub</a>!</p>
+                        </section>
                     </footer>
-                    <section className="flex flex-col gap-4 px-6 text-sm text-justify text-gray-700">
-                        <h2 className="text-md text-gray-600 font-bold">About SpaceBadgers</h2>
-                        <p>Hey there, welcome to SpaceBadgers! If you&#x27;re wondering who we are and what we do, let us fill you in. We&#x27;re an open-source project with a passion for delivering top-notch SVG badges that are as speedy as they are stylish.</p>
-                        <p>You see, we noticed that developers and project maintainers like you needed a way to display key information &#x2013; think build status, version, download counts &#x2013; in a way that&#x27;s quick and easy to integrate. That&#x27;s where we come in.</p>
-                        <p>Under the hood, we&#x27;re powered by a Rust-based core library <a className="text-xs text-gray-700 hover:text-orange-600" href="https://crates.io/crates/spacebadgers" target="_blank">(crates.io)</a> and a Cloudflare worker. This dynamic duo ensures that we&#x27;re always delivering badges with excellent performance, straight from the edge. And speaking of integration, we&#x27;ve made it our mission to ensure smooth sailing when it comes to working with third-party services, courtesy of our NextJS-based API routes running on Vercel edge infrastructure.</p>
-                        <p>What makes us different? We&#x27;re glad you asked! Unlike traditional image-based badges, we&#x27;re all about SVGs. They&#x27;re scalable, high-quality, and we serve them in a minified and compressed form for optimal delivery.</p>
-                        <p>So that&#x27;s us, SpaceBadgers, in a nutshell. We&#x27;re here to provide a seamless, efficient badge generation process. If that sounds like your cup of tea, why not join the open-source community using SpaceBadgers today? We&#x27;d be thrilled to have you onboard!</p>
-                        <p>For more information, take a look at our <a className="text-gray-700 hover:text-orange-600 underline underline-offset-2" href="https://github.com/splittydev/spacebadgers" target="_blank">GitHub</a>!</p>
-                    </section>
                 </div>
             </div>
             <Analytics />
