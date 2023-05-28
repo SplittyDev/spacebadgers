5b40f52c4d62cee64ab2554c15651b56bfd478a0
SplittyDev <splittydev@gmail.com>
2023-05-28 03:32:01 +0200

    Add `/json/icons` endpoint

--- badgers/src/icons/icon_set.rs
+++ badgers/src/icons/icon_set.rs
@@ -1,5 +1,7 @@
 use phf::Map;
 
+#[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize))]
 pub struct IconSet {
     pub name: &'static str,
     pub icons: Map<&'static str, &'static str>,
