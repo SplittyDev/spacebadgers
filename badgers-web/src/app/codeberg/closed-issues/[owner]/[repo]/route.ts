442be75ea6b0b165da17f20a9a0d02e5e61a14c5
Marcin Puc <tranzystorek.io@protonmail.com>
2024-01-13 19:19:08 +0100

    Add Codeberg issues endpoints

---
+++ badgers-web/src/app/codeberg/closed-issues/[owner]/[repo]/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest } from "next/server"
+
+import Badge from '@/utils/Badge'
+import Codeberg from '@/utils/Codeberg'
+
+interface Params {
+    params: {
+        owner: string
+        repo: string
+    }
+}
+
+export async function GET(request: NextRequest, { params: { owner, repo } }: Params) {
+    const closedIssuesCount = await Codeberg.getClient().getIssuesCount({ owner, repo }, { type: 'issues', state: 'closed' })
+
+    return await Badge.generate(request, 'closed issues', closedIssuesCount?.toString() ?? 'None')
+}
+
+export const runtime = 'edge'
---
+++ badgers-web/src/app/codeberg/open-issues/[owner]/[repo]/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest } from "next/server"
+
+import Badge from '@/utils/Badge'
+import Codeberg from '@/utils/Codeberg'
+
+interface Params {
+    params: {
+        owner: string
+        repo: string
+    }
+}
+
+export async function GET(request: NextRequest, { params: { owner, repo } }: Params) {
+    const openIssuesCount = await Codeberg.getClient().getIssuesCount({ owner, repo }, { type: 'issues', state: 'open' })
+
+    return await Badge.generate(request, 'open issues', openIssuesCount?.toString() ?? 'None')
+}
+
+export const runtime = 'edge'
