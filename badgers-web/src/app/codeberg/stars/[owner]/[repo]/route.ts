28798ea23c947db8d733330b72b5b92d594635db
SplittyDev <splittydev@protonmail.com>
2024-03-07 01:33:31 +0100

    web: Add codeberg stars endpoint

---
+++ badgers-web/src/app/codeberg/stars/[owner]/[repo]/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest } from 'next/server'
+
+import Badge from '@/utils/Badge'
+import Codeberg from '@/utils/Codeberg'
+
+interface Params {
+    params: {
+        owner: string
+        repo: string
+    }
+}
+
+export async function GET(request: NextRequest, { params: { owner, repo } }: Params) {
+    const repository = await Codeberg.getClient().getRepository({ owner, repo })
+    const stargazers = repository?.stars_count
+
+    return await Badge.generate(request, 'stars', stargazers?.toString() ?? 'None', {
+        color: !!stargazers ? 'blue' : 'yellow'
+    })
+}
+
+export const runtime = 'edge'
